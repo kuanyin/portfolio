90914782446b3aa38088ca0b3614c47e71e65a5b
kuanyin <becky810210@gmail.com>
2017-06-01 16:06:02 +0800

    rwd

--- javascripts/jsmanage.js
+++ javascripts/jsmanage.js
@@ -2,5 +2,29 @@ $(function(){
     $('.goTop').click(function(){
 			$('body,html').stop().animate({scrollTop:0});
 			return false;
+	});
+	//mobile menu
+		$('.m_menu').find('a.main').click(function(){
+			if(!$(this).parents('.m_menu').hasClass('active')){
+				$(this).parents('.m_menu').addClass('active');
+				$(this).find('i').addClass('fa-times').removeClass('fa-reorder');
+				$('.m_menu').find('.mask').fadeIn(100);
+				$('.m_menu').find('.hideBox').fadeIn(100);
+				$('body').css('overflow','hidden');
+				$('.m_menu').find('.mask').click(function(){/*點空白處收起menu*/
+					$('.m_menu').removeClass('active');
+					$('.m_menu').find('.hideBox').fadeOut();
+					$('.m_menu').find('.mask').fadeOut();
+				});
+			}else{
+				$(this).parents('.m_menu').removeClass('active');
+				$(this).find('i').addClass('fa-reorder').removeClass('fa-times');
+				$('.m_menu').find('.mask').fadeOut();
+				$('.m_menu').find('.hideBox').fadeOut();
+				$('body').css('overflow','auto');
+			}//end if hasClass
+						
+			return false;
 		});
+		
 });
